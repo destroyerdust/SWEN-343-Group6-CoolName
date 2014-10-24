9cafed2cbf6242f44949b144d056716fab6b01c8
CCraig <clc1774@g.rit.edu>
2014-10-24 11:21:19 -0400

    Minor changes to RideEntryDAO syntax. First implementation of findPassengersForDriver() is written.

--- src/main/java/com/thumbsup/coolnamecli/RideMatchMaker.java
+++ src/main/java/com/thumbsup/coolnamecli/RideMatchMaker.java
@@ -2,7 +2,13 @@ package com.thumbsup.coolnamecli;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.thumbsup.coolnamecli.dao.RideEntryDAO;
+import com.thumbsup.coolnamecli.dao.SessionFactory;
 import com.thumbsup.coolnamecli.entity.RideEntry;
+import com.thumbsup.coolnamecli.entity.User;
 
 /**
  * This class contains methods that match Drivers with Passengers.
@@ -12,11 +18,11 @@ import com.thumbsup.coolnamecli.entity.RideEntry;
  * */
 public class RideMatchMaker {
 
-	/* private RideEntryDAO = null */
+	private RideEntryDAO red;
 
-	public RideMatchMaker(/* RideEntryDAO where */) {
+	public RideMatchMaker(RideEntryDAO red) {
 		// setup RideEntryDAO
-		// this.RideEntryDAO = red
+		this.red = red;
 	}
 
 	/**
@@ -31,7 +37,7 @@ public class RideMatchMaker {
 	 *         system that fit and need the Driver.
 	 * 
 	 * */
-	private List<RideEntry> findPassengersForDriver(Driver d) {
+	private List<Object> findPassengersForDriver(User driver) {
 		//Hard-coded SQL query (Hibernate form)
 		
 		/* 
@@ -40,18 +46,19 @@ public class RideMatchMaker {
 		 *  2) Selectively pick from the RideEntries those that do not have a vehicle ID 
 		 *  3) Discriminate the RideEntries by Group and Location
 		 * 
-		 * Potential code:
-		 *  String hql = "FROM RideEntry RE WHERE RE.VehicleID == null";
-		 *  Query query = session.createQuery(hql);
-		 *  List results = query.list();
-		 * */
+		 */  
+		SessionFactory factory = SessionFactory.getSessionFactory();
+		Session session = factory.getSession();
+		String hql = "FROM RideEntry RE WHERE RE.VehicleID == null";
+		Query query = session.createQuery(hql);
+		List<Object> results = query.list();
 		
 		//return result
-		return null;
+		return results;
 	}
 
 	/* Match Passengers to Drivers */
-	private List<RideEntry> findDriversForPassenger(Passenger p) {
+	private List<RideEntry> findDriversForPassenger(User p) {
 		
 		//Please reference the findPassengersForDriver method for how to fill this out.
 		return null;
--- src/main/java/com/thumbsup/coolnamecli/dao/RideEntryDAO.java
+++ src/main/java/com/thumbsup/coolnamecli/dao/RideEntryDAO.java
@@ -8,74 +8,67 @@ import org.hibernate.Transaction;
 
 import com.thumbsup.coolnamecli.entity.RideEntry;
 
-public class RideEntryDAO extends CRUDManager<RideEntry, Integer>
-{
-	private static SessionFactory SessionFactory;
+public class RideEntryDAO extends CRUDManager<RideEntry, Integer> {
 
 	@Override
-	public RideEntry insert(RideEntry entry)
-	{
-		Session s = SessionFactory.getSession();
+	public RideEntry insert(RideEntry entry) {
+		SessionFactory factory = SessionFactory.getSessionFactory();
+		Session s = factory.getSession();
 		Transaction t = s.beginTransaction();
 		s.save(entry);
 		t.commit();
 		s.flush();
-		entry = (RideEntry)s.load(RideEntry.class, entry);
-        s.close();
-        
-        return entry;
-	}
-
-	@Override
-	public RideEntry select(Integer rideEntryID)
-	{
-		SessionFactory factory = SessionFactory.getSessionFactory();
-		Session s = factory.getSession();
-		RideEntry entry = (RideEntry)s.load(RideEntry.class, rideEntryID);
+		entry = (RideEntry) s.load(RideEntry.class, entry);
 		s.close();
-		
+
 		return entry;
 	}
 
 	@Override
-	public List<RideEntry> selectAll()
-	{
+	public RideEntry select(Integer rideEntryID) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-        List<RideEntry> entryList = new ArrayList<RideEntry>(); 
-		entryList = s.createCriteria(RideEntry.class).list();              
-        s.close();
-        
-        return entryList;
+		RideEntry entry = (RideEntry) s.load(RideEntry.class, rideEntryID);
+		s.close();
+
+		return entry;
 	}
 
 	@Override
-	public RideEntry delete(RideEntry entry)
-	{
+	public List<RideEntry> selectAll() {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-		Transaction transaction = s.beginTransaction();    	
-    	s.delete(entry);
-    	transaction.commit();
-    	s.flush();
-    	entry = (RideEntry)s.load(RideEntry.class, entry.getRideEntryID());
-    	s.close();
-    	
-    	return entry;
+		List<RideEntry> entryList = new ArrayList<RideEntry>();
+		entryList = s.createCriteria(RideEntry.class).list();
+		s.close();
+
+		return entryList;
 	}
 
 	@Override
-	public RideEntry update(RideEntry entry)
-	{
+	public RideEntry delete(RideEntry entry) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-    	Transaction transaction = s.beginTransaction();
-    	s.update(entry);
-    	transaction.commit();
-    	entry = (RideEntry) s.load(RideEntry.class, entry.getRideEntryID());
-    	s.close();
-    	return entry;
+		Transaction transaction = s.beginTransaction();
+		s.delete(entry);
+		transaction.commit();
+		s.flush();
+		entry = (RideEntry) s.load(RideEntry.class, entry.getRideEntryID());
+		s.close();
+
+		return entry;
 	}
-	
+
+	@Override
+	public RideEntry update(RideEntry entry) {
+		SessionFactory factory = SessionFactory.getSessionFactory();
+		Session s = factory.getSession();
+		Transaction transaction = s.beginTransaction();
+		s.update(entry);
+		transaction.commit();
+		entry = (RideEntry) s.load(RideEntry.class, entry.getRideEntryID());
+		s.close();
+		return entry;
+	}
 
 }
