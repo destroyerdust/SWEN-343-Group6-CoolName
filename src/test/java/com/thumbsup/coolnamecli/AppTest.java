30652cc011afa6c62cd3ee1c04795558da8ed0f9
igMoreira <iago.silva@al.infnet.edu.br>
2014-10-24 17:22:29 -0700

    Add RideEntryDAOTest in Test Suite AppTest

--- src/test/java/com/thumbsup/coolnamecli/AppTest.java
+++ src/test/java/com/thumbsup/coolnamecli/AppTest.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UserDAOTest.class })
+@SuiteClasses({ UserDAOTest.class,
+				RideEntryDAOTest.class})
 public class AppTest {
 
 }
