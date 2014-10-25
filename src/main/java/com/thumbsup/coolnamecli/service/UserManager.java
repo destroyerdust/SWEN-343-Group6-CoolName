6b07bb341252ab1ca19304c737a851d6333cb5cb
Sean Barkley <sean@seanbarkley.com>
2014-10-25 15:28:12 -0400

    Updated UserManager  to work with DAO's

--- src/main/java/com/thumbsup/coolnamecli/service/UserManager.java
+++ src/main/java/com/thumbsup/coolnamecli/service/UserManager.java
@@ -1,98 +1,81 @@
 package com.thumbsup.coolnamecli.service;
 
-import java.util.Iterator;
-import java.util.List;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
+import com.thumbsup.coolnamecli.dao.UserDAO;
 import com.thumbsup.coolnamecli.entity.User;
 
 public class UserManager {
-	private static SessionFactory factory;
-	
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
-		try{
-			factory = new Configuration().configure().buildSessionFactory();
-		}catch (Throwable ex) {
-			System.err.println("Failed to create sessionFactory object." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-		
-		UserManager MU = new UserManager();
-		
-		@SuppressWarnings("unused")
-		Integer UserID1 = MU.addUser(0, "Bob", "Builder", "5555555555", "BobBuilder01");
-		Integer UserID2 = MU.addUser(0, "Clark", "Clenton", "5555555552", "1231231");
-		
-		//MU.listUsers();
-		
-		//MU.deleteUser(UserID2);
-		
-		MU.listUsers();
-		
-	}
-	
-	
-	public Integer addUser(int userType, String fname, String lname, String phoneNumber, String password) {
-		Session session = factory.openSession();
-		Transaction tx = null;
-		Integer userId = null;
-		try {
-			tx = session.beginTransaction();
-			User user = new User(userType, fname, lname, phoneNumber, password);
-			userId = (Integer) session.save(user);
-			tx.commit();
-		} catch  (HibernateException e) {
-	         if (tx!=null) tx.rollback();
-	         e.printStackTrace(); 
-	      }finally {
-	         session.close(); 
-	      }
+
+	private UserDAO userDAO = new UserDAO();
+
+	/**
+	 * Creates the user object to send to the userDAO
+	 * 
+	 * @param userName
+	 * @param firstName
+	 * @param lastName
+	 * @param password
+	 * @param passwordSalt
+	 * @param phoneNumber
+	 * @param userType
+	 * @return
+	 */
+	public Integer createUser(String userName, String firstName,
+			String lastName, String password, String passwordSalt,
+			String phoneNumber, int userType) {
+		int userId = 0;
+
+		User u = new User();
+		u.setUserName(userName);
+		u.setFirstName(firstName);
+		u.setLastName(lastName);
+		u.setPassword(password);
+		u.setPasswordSalt(passwordSalt);
+		u.setPhoneNumber(phoneNumber);
+		u.setUserType(userType);
+
+		userDAO.insert(u);
+
 		return userId;
 	}
-	
-	public void listUsers() {
-		Session session = factory.openSession();
-		Transaction tx = null;
+
+	/**
+	 * Selects the user from the userId and then updates the user's fields
+	 * that a user can select
+	 * 
+	 * @param userId
+	 * @param userName
+	 * @param firstName
+	 * @param lastName
+	 * @param password
+	 * @param phoneNumber
+	 * @param userType
+	 */
+	public void updateUser(int userId, String userName, String firstName,
+			String lastName, String password, String phoneNumber, int userType) {
 		
-		try {
-			tx = session.beginTransaction();
-			@SuppressWarnings("unchecked")
-			List<User> users = session.createQuery("FROM User U where U.lastName = 'Builder'").list();
-			for (Iterator<User> iterator = users.iterator(); iterator.hasNext();){
-				User user = (User) iterator.next();
-				System.out.println("First Name: " + user.getFirstName());
-				System.out.println("Last Name: " + user.getLastName());
-				System.out.println("Phone Number: " + user.getPhoneNumber());
-			}
-			tx.commit();
-		}catch (HibernateException e) {
-	         if (tx!=null) tx.rollback();
-	         e.printStackTrace(); 
-	      }finally {
-	         session.close(); 
-	      }
+		User u = userDAO.select(userId);
+		u.setFirstName(firstName);
+		u.setLastName(lastName);
+		u.setPassword(password);
+		u.setPhoneNumber(phoneNumber);
+		u.setUserType(userType);
+		
+		userDAO.update(u);
 	}
-	
-	public void deleteUser(Integer UserId) {
-		 Session session = factory.openSession();
-	     Transaction tx = null;
-	     try {
-	    	 tx = session.beginTransaction();
-	    	 User user = (User) session.get(User.class, UserId);
-	    	 session.delete(user);;
-	    	 tx.commit();
-	     }catch (HibernateException e) {
-	         if (tx!=null) tx.rollback();
-	         e.printStackTrace(); 
-	      }finally {
-	         session.close(); 
-	      }
+
+	/**
+	 * Selects the user based off the userId and tells the userDAO to delete
+	 * 
+	 * @param userId
+	 */
+	public void deleteUser(int userId) {
+		
+		// Select user to delete
+		User u = userDAO.select(userId);
+		
+		// Delete User
+		userDAO.delete(u);
+
 	}
 
 }
