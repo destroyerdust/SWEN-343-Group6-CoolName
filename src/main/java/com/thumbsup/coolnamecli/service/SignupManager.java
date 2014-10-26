bb14efe70effc1466227eb2d763c5def240e3d73
CCraig <clc1774@g.rit.edu>
2014-10-26 02:02:10 -0400

    Implemented SignupManager.java, first cut.

---
+++ src/main/java/com/thumbsup/coolnamecli/service/SignupManager.java
@@ -0,0 +1,51 @@
+package com.thumbsup.coolnamecli.service;
+
+import com.sun.jmx.snmp.Timestamp;
+import com.thumbsup.coolnamecli.dao.SignupDAO;
+import com.thumbsup.coolnamecli.entity.Signup;
+import com.thumbsup.coolnamecli.entity.User;
+
+/**
+ * Contributors: Conor Craig
+ * 
+ * Class owns a SignupDAO and manipulates its functionality.
+ * 
+ * */
+public class SignupManager {
+
+	private SignupDAO sDAO = new SignupDAO();
+
+	public int createSignup(int rideOnId, int userId, int rideEntryId,
+			Timestamp time) {
+
+		Signup s = sDAO.select(rideOnId);
+		s.setRideOnID(rideOnId);
+		s.setUserID(userId);
+		s.setRideEntryID(rideEntryId);
+
+		sDAO.insert(s);
+
+		return rideOnId;
+	}
+
+	public void updateSignup(int rideOnId, int userId, int rideEntryId,
+			Timestamp time) {
+
+		Signup s = sDAO.select(rideOnId);
+		s.setRideOnID(rideOnId);
+		s.setUserID(userId);
+		s.setRideEntryID(rideEntryId);
+
+		sDAO.update(s);
+	}
+
+	public void deleteSignup(int rideOnId) {
+
+		// Select user to delete
+		Signup s = sDAO.select(rideOnId);
+
+		// Delete User
+		sDAO.delete(s);
+
+	}
+}
