93a579aca0097cc97a2dc49eefc6c96adfbccfda
igMoreira <iago.silva@al.infnet.edu.br>
2014-11-09 12:55:28 -0800

    Commiting the logging process to use session

--- src/main/java/com/thumbsup/coolname/LoginController.java
+++ src/main/java/com/thumbsup/coolname/LoginController.java
@@ -1,11 +1,10 @@
 package com.thumbsup.coolname;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +28,6 @@ public class LoginController {
 			HttpServletResponse response, HttpServletRequest request)
 	{
 		String page = "";
-		Cookie cookie = null; 
 		if(username.equals("") || password.equals(""))
 		{
 			page = "login";
@@ -47,9 +45,9 @@ public class LoginController {
 			}
 			else
 			{
-				cookie = new Cookie("auth", u.getUserId().toString());
 				page = "home";
-				response.addCookie(cookie);
+				HttpSession session = request.getSession();
+				session.setAttribute("auth", u.getUserId());
 			}
 		}
 		
