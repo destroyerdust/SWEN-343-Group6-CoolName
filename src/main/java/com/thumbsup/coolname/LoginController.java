18bf1888d627c885fff26c39d461d50ab57e404b
CCraig <clc1774@g.rit.edu>
2014-11-09 19:12:34 -0500

    **This is the login/logout commit that was annihilated because no one could connect to hibernate.** The login/logout/signup section now displays properly with respect to session authorization state.

--- src/main/java/com/thumbsup/coolname/LoginController.java
+++ src/main/java/com/thumbsup/coolname/LoginController.java
@@ -16,48 +16,42 @@ import com.thumbsup.coolname.service.UserManager;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/account/login", method=RequestMethod.GET)
-	public String getLogin(HttpServletRequest request)
-	{
+	@RequestMapping(value = "/account/login", method = RequestMethod.GET)
+	public String getLogin(HttpServletRequest request) {
 		request.setAttribute("l", true);
 		return "login";
 	}
-	
-	@RequestMapping(value="/account/login", method=RequestMethod.POST)
-	public ModelAndView postLogin(@RequestParam(value="usr", required=true, defaultValue="") String username,
-			@RequestParam(value="psw", required=true, defaultValue="") String password,
-			HttpServletRequest request)
-	{
+
+	@RequestMapping(value = "/account/login", method = RequestMethod.POST)
+	public ModelAndView postLogin(
+			@RequestParam(value = "usr", required = true, defaultValue = "") String username,
+			@RequestParam(value = "psw", required = true, defaultValue = "") String password,
+			HttpServletRequest request) {
 		String page = "";
-		if(username.equals("") || password.equals(""))
-		{
+		if (username.equals("") || password.equals("")) {
 			page = "login";
 			request.setAttribute("l", false);
-		}
-		else
-		{
+		} else {
 			UserManager manager = new UserManager();
 			User u = manager.login(username, password);
 			password = null;
-			if( u == null)
-			{
+			if (u == null) {
 				page = "login";
 				request.setAttribute("l", false);
-			}
-			else
-			{
+			} else {
 				page = "redirect:/";
 				HttpSession session = request.getSession();
 				session.setAttribute("auth", u.getUserId());
 			}
 		}
-		
+
 		return new ModelAndView(page);
 	}
-	
-	@RequestMapping(value="/account/logout", method=RequestMethod.GET)
-	public String fireLogout(HttpServletRequest request){
+
+	@RequestMapping(value = "/account/logout", method = RequestMethod.GET)
+	public ModelAndView fireLogout(HttpServletRequest request) {
 		request.getSession().invalidate();
-	    return "home";
+
+		return new ModelAndView("redirect:/");
 	}
 }
