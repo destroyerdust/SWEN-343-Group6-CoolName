73ffbfac4dbdbcdcb3b2f1ff041f9cfbbd25c410
AaronDam <AaronDamrau@gmail.com>
2014-11-23 15:20:33 -0500

    Pretty pretty group page.

--- src/main/java/com/thumbsup/coolname/RideController.java
+++ src/main/java/com/thumbsup/coolname/RideController.java
@@ -11,7 +11,7 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
+//import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
