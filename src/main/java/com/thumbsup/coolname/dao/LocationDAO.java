e6e975bd8f1ec3c70064b6ee6b083e19f43dc434
Conor Craig <cshdfree@gmail.com>
2014-12-03 00:44:00 -0500

    Added Google Maps location selection to RideCreate page. Modified CreateRide() in RideController to make use of LocationManager and actually create Location rows in the Location table. Added google maps api to includes.jsp. Modified rideCreate.jsp to include the google map and its supporting selector/marker script.

--- src/main/java/com/thumbsup/coolname/dao/LocationDAO.java
+++ src/main/java/com/thumbsup/coolname/dao/LocationDAO.java
@@ -3,10 +3,10 @@ package com.thumbsup.coolname.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import com.thumbsup.coolname.dao.SessionFactory;
 import com.thumbsup.coolname.entity.Location;
 
 public class LocationDAO extends CRUDManager<Location, Integer> {
@@ -15,20 +15,21 @@ public class LocationDAO extends CRUDManager<Location, Integer> {
 	public Location insert(Location location) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-		Transaction t = s.beginTransaction();        
+		Transaction t = s.beginTransaction();
 		s.save(location);
-        t.commit();
-        s.flush();
-        Location resultLocation = (Location)s.get(Location.class, location.getLocationID());
-        s.close();
-        return resultLocation;
+		t.commit();
+		s.flush();
+		Location resultLocation = (Location) s.get(Location.class,
+				location.getLocationID());
+		s.close();
+		return resultLocation;
 	}
 
 	@Override
 	public Location select(Integer pk) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-		Location l = (Location)s.get(Location.class, pk);
+		Location l = (Location) s.get(Location.class, pk);
 		return l;
 	}
 
@@ -38,7 +39,7 @@ public class LocationDAO extends CRUDManager<Location, Integer> {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
 		List<Location> l = new ArrayList<Location>();
-        l = s.createCriteria(Location.class).list();
+		l = s.createCriteria(Location.class).list();
 		return l;
 	}
 
@@ -46,26 +47,46 @@ public class LocationDAO extends CRUDManager<Location, Integer> {
 	public Location delete(Location location) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-    	Transaction transaction = s.beginTransaction();
-    	s.delete(location);
-    	transaction.commit();
-    	s.flush();
-    	location = (Location)s.get(Location.class, location.getLocationID());
-    	s.close();
-    	return location;
+		Transaction transaction = s.beginTransaction();
+		s.delete(location);
+		transaction.commit();
+		s.flush();
+		location = (Location) s.get(Location.class, location.getLocationID());
+		s.close();
+		return location;
 	}
 
 	@Override
 	public Location update(Location location) {
 		SessionFactory factory = SessionFactory.getSessionFactory();
 		Session s = factory.getSession();
-    	Transaction transaction = s.beginTransaction();
-    	s.update(location);
-    	transaction.commit();
-    	s.flush();
-    	Location resultLocation = (Location) s.get(Location.class, location.getLocationID());
-    	s.close();
-    	return resultLocation;
+		Transaction transaction = s.beginTransaction();
+		s.update(location);
+		transaction.commit();
+		s.flush();
+		Location resultLocation = (Location) s.get(Location.class,
+				location.getLocationID());
+		s.close();
+		return resultLocation;
 	}
 
+	/*
+	 * Method goes into the Location table and returns the locations that
+	 * correspond to the given RideEntryID
+	 */
+	public List<Location> findLocationFromRideEntryId(int rideEntryID) {
+
+		SessionFactory factory = SessionFactory.getSessionFactory();
+		Session s = factory.getSession();
+
+		Query query = s
+		// .createQuery("select locations from Location location where location.rideEntryID = :parameterRideEntryID");
+				.createQuery("FROM Location as l where l.rideEntryID = :parameterRideEntryID");
+		query.setParameter("" + "parameterRideEntryID", rideEntryID);
+		List<Location> locations = query.list();
+
+		s.flush();
+		s.close();
+		return locations;
+	}
 }
--- src/main/java/com/thumbsup/coolname/service/LocationManager.java
+++ src/main/java/com/thumbsup/coolname/service/LocationManager.java
@@ -7,58 +7,59 @@ import com.thumbsup.coolname.entity.Location;
 
 public class LocationManager {
 
-private LocationDAO locationDAO = new LocationDAO();
+	private LocationDAO locationDAO = new LocationDAO();
 
-//create location
-public Location createlocation(String title, double latitude, double longitude)
-{
-Location location = new Location();
+	// create location
+	public Location createlocation(String title, double latitude,
+			double longitude, int rideEntryID) {
+		Location location = new Location();
 
-location.setTitle(title);
-location.setLatitude(latitude);
-location.setLongitude(longitude);
+		location.setTitle(title);
+		location.setRideEntryID(rideEntryID);
+		location.setLatitude(latitude);
+		location.setLongitude(longitude);
 
-location = locationDAO.insert(location);
+		location = locationDAO.insert(location);
 
-return location;
-}
-//update location
-public int updatelocation(int locationID, String title, double latitude, double longitude)
-{
-Location location = locationDAO.select(locationID);
+		return location;
+	}
 
-location.setTitle(title);
-location.setLatitude(latitude);
-location.setLongitude(longitude);
+	// update location
+	public int updatelocation(int locationID, String title, double latitude,
+			double longitude, int rideEntryID) {
+		Location location = locationDAO.select(locationID);
 
-locationDAO.update(location);
+		location.setTitle(title);
+		location.setLatitude(latitude);
+		location.setLongitude(longitude);
+		location.setRideEntryID(rideEntryID);
 
-return locationID;
-}
-//deletelocation
-public void deletelocation(int locationID)
-{
-Location location= locationDAO.select(locationID);
+		locationDAO.update(location);
 
-locationDAO.delete(location);
-}
-//listlocations
-public List<Location> listlocations()
-{
-List<Location> locations = locationDAO.selectAll();
+		return locationID;
+	}
 
-return locations;
-}
+	// deletelocation
+	public void deletelocation(int locationID) {
+		Location location = locationDAO.select(locationID);
 
-public Location selectlocation(int locationID)
-{
-return locationDAO.select(locationID);
-}
+		locationDAO.delete(location);
+	}
 
-//Search the Location Table for Locations that contain the RideEntryID
-public List<Location> findLocationFromRideEntryId(int RideEntryID)
-{
-return locationDAO.findLocationFromRideEntryId(RideEntryID);
-}
+	// listlocations
+	public List<Location> listlocations() {
+		List<Location> locations = locationDAO.selectAll();
+
+		return locations;
+	}
+
+	public Location selectlocation(int locationID) {
+		return locationDAO.select(locationID);
+	}
+
+	// Search the Location Table for Locations that contain the RideEntryID
+	public List<Location> findLocationFromRideEntryId(int RideEntryID) {
+		return locationDAO.findLocationFromRideEntryId(RideEntryID);
+	}
 
 }
